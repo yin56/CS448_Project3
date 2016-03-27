599c9db45424813df410b9bc1a581dd8ed8005ad
yin56 <yin56@purdue.edu>
2016-03-27 14:56:22 -0400

    Update Projection.java

--- CS448_Spring_2016_Proj3_CodeSkeleton/RelOperator/src/relop/Projection.java
+++ CS448_Spring_2016_Proj3_CodeSkeleton/RelOperator/src/relop/Projection.java
@@ -1,16 +1,27 @@
-package relop;
+	package relop;
 
 /**
  * The projection operator extracts columns from a relation; unlike in
  * relational algebra, this operator does NOT eliminate duplicate tuples.
  */
 public class Projection extends Iterator {
+	
+	private Integer field[];
+	private Iterator it;
+	private Tuple next;
+	private boolean start = true;
+	private boolean consumed;
+	private Tuple proj;
 
   /**
    * Constructs a projection, given the underlying iterator and field numbers.
    */
   public Projection(Iterator iter, Integer... fields) {
-    throw new UnsupportedOperationException("Not implemented");
+	  System.out.print("Constructing Projection\n");
+	  this.schema = iter.getSchema();
+	  this.field = fields;
+	  this.it = iter;
+    //throw new UnsupportedOperationException("Not implemented");
   }
 
   /**
@@ -18,35 +29,59 @@ public class Projection extends Iterator {
    * child iterators, and increases the indent depth along the way.
    */
   public void explain(int depth) {
-    throw new UnsupportedOperationException("Not implemented");
+	  System.out.print("Projection\n");
+    //throw new UnsupportedOperationException("Not implemented");
   }
 
   /**
    * Restarts the iterator, i.e. as if it were just constructed.
    */
   public void restart() {
-    throw new UnsupportedOperationException("Not implemented");
+	  it.restart();
+	  consumed = true;
+    //throw new UnsupportedOperationException("Not implemented");
   }
 
   /**
    * Returns true if the iterator is open; false otherwise.
    */
   public boolean isOpen() {
-    throw new UnsupportedOperationException("Not implemented");
+	  if(it.isOpen() == true){
+		  return true;
+	  }
+	  else{
+		  return false;
+	  }
+    //throw new UnsupportedOperationException("Not implemented");
   }
 
   /**
    * Closes the iterator, releasing any resources (i.e. pinned pages).
    */
   public void close() {
-    throw new UnsupportedOperationException("Not implemented");
+	  it.close();
+    //throw new UnsupportedOperationException("Not implemented");
   }
 
   /**
    * Returns true if there are more tuples, false otherwise.
    */
   public boolean hasNext() {
-    throw new UnsupportedOperationException("Not implemented");
+	  //next.s
+	 proj = new Tuple(schema);
+	  //result.print();
+	  while(it.hasNext()){
+		  next = it.getNext();
+		  for(int i = 0; i < field.length; i++){
+			  //System.out.print(next.getField(field[i]).getClass().toString() +"\n");
+			 proj.setField(field[i], next.getField(field[i])); 
+		  }
+		  //proj = result;
+		  consumed = true;
+		  return true;
+	  }
+	  return false;
+    //throw new UnsupportedOperationException("Not implemented");
   }
 
   /**
@@ -55,7 +90,8 @@ public class Projection extends Iterator {
    * @throws IllegalStateException if no more tuples
    */
   public Tuple getNext() {
-    throw new UnsupportedOperationException("Not implemented");
+	  return proj;
+    //throw new UnsupportedOperationException("Not implemented");
   }
 
 } // public class Projection extends Iterator
