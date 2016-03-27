2eaef15e8510020c3b09a8e20e2661bb23f342d5
yin56 <yin56@purdue.edu>
2016-03-27 15:53:26 -0400

    Update IndexScan.java

--- CS448_Spring_2016_Proj3_CodeSkeleton/RelOperator/src/relop/IndexScan.java
+++ CS448_Spring_2016_Proj3_CodeSkeleton/RelOperator/src/relop/IndexScan.java
@@ -1,19 +1,40 @@
+
 package relop;
 
+import global.RID;
 import global.SearchKey;
 import heap.HeapFile;
+import index.BucketScan;
 import index.HashIndex;
 
 /**
  * Wrapper for bucket scan, an index access method.
  */
 public class IndexScan extends Iterator {
+	
+	HashIndex index;
+	HeapFile file;
+	boolean open = false;
+	private BucketScan scan;
+	private SearchKey key;
+	boolean consumed;
+	int hash;
+	Tuple next;
+	boolean noMore = false;
+	int numBuckets = 0;
 
   /**
    * Constructs an index scan, given the hash index and schema.
    */
   public IndexScan(Schema schema, HashIndex index, HeapFile file) {
-    throw new UnsupportedOperationException("Not implemented");
+	  this.schema = schema;
+	  this.index = index;
+	  this.file = file;
+	  this.scan = index.openScan();
+	  consumed = true;
+	  
+	  
+    //throw new UnsupportedOperationException("Not implemented");
   }
 
   /**
@@ -21,35 +42,70 @@ public class IndexScan extends Iterator {
    * child iterators, and increases the indent depth along the way.
    */
   public void explain(int depth) {
-    throw new UnsupportedOperationException("Not implemented");
+	  System.out.print("IndexScan\n");
+    //throw new UnsupportedOperationException("Not implemented");
   }
 
   /**
    * Restarts the iterator, i.e. as if it were just constructed.
    */
   public void restart() {
-    throw new UnsupportedOperationException("Not implemented");
+	  if(isOpen() == true){
+		  close();
+		  scan = index.openScan();
+		  open = true;
+	  }
+	  else{
+		  scan = index.openScan();
+		  open = true;
+	  }
+    //throw new UnsupportedOperationException("Not implemented");
   }
 
   /**
    * Returns true if the iterator is open; false otherwise.
    */
   public boolean isOpen() {
-    throw new UnsupportedOperationException("Not implemented");
+	  if(open == true){
+		  return true;
+	  }
+	  else{
+		  return false;
+	  }
+    //throw new UnsupportedOperationException("Not implemented");
   }
 
   /**
    * Closes the iterator, releasing any resources (i.e. pinned pages).
    */
   public void close() {
-    throw new UnsupportedOperationException("Not implemented");
+	  scan.close();
+	  open = false;
+    //throw new UnsupportedOperationException("Not implemented");
   }
 
   /**
    * Returns true if there are more tuples, false otherwise.
    */
   public boolean hasNext() {
-    throw new UnsupportedOperationException("Not implemented");
+	  if(scan.hasNext() == true){
+		  noMore = false;
+		  RID record = new RID();
+		  record = scan.getNext();
+		  //file.selectRecord(record);
+		  Tuple tup = new Tuple(schema, file.selectRecord(record));
+		  hash = scan.getNextHash();
+		  //hash = nhash;
+		  key = scan.getLastKey();
+		  numBuckets++;
+		  next = tup;
+		  return true;
+	  }
+	  else{
+		  noMore = true;
+		  return false;
+	  }
+    //throw new UnsupportedOperationException("Not implemented");
   }
 
   /**
@@ -58,14 +114,22 @@ public class IndexScan extends Iterator {
    * @throws IllegalStateException if no more tuples
    */
   public Tuple getNext() {
-    throw new UnsupportedOperationException("Not implemented");
+	  if(hasNext() == true){
+		 consumed = true;
+		 return next;
+	  }
+	  else{
+		  throw new IllegalStateException("NO MORE TUPLES");
+	  }
+    //throw new UnsupportedOperationException("Not implemented");
   }
 
   /**
    * Gets the key of the last tuple returned.
    */
   public SearchKey getLastKey() {
-    throw new UnsupportedOperationException("Not implemented");
+	  return  key;
+    //throw new UnsupportedOperationException("Not implemented");
   }
 
   /**
@@ -73,7 +137,13 @@ public class IndexScan extends Iterator {
    * number of buckets if none.
    */
   public int getNextHash() {
-    throw new UnsupportedOperationException("Not implemented");
+	  if(noMore == false){
+		  return hash;
+	  }
+	  else{
+		  return numBuckets;
+	  }
+    //throw new UnsupportedOperationException("Not implemented");
   }
 
 } // public class IndexScan extends Iterator
